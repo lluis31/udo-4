73a2e304d41ff429979f55730ff9b1c9178b19a7
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 12:47:46 +0100

    ex4

---
+++ ex4-udo4/ex4-udo4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex4_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el primer numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escibre el segundo numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.WriteLine("El primer numero es mayor");
+            else
+                Console.WriteLine("El segundo numero es mayor");
+
+            Console.ReadLine();
+        }
+    }
+}
