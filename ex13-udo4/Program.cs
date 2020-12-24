4745b721bc1f96a76c835cdf757e153cc94bf4c1
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 14:42:52 +0100

    ex13

---
+++ ex13-udo4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex13_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Introduce tu nombre: "); nombre = Console.ReadLine();
+            if (nombre == "Lluis")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Bienvenido Lluis");
+            }
+        }
+    }
+}
