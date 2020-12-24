ed12b8b64485d3fe2fe2a270e75e65851e97f05c
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 13:34:43 +0100

    ex8

---
+++ ex8-udo4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex8_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (a <= 100)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
