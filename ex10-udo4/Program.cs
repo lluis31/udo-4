5e32e00914a5942903e7f2dba160bbc46d4b7493
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 13:55:31 +0100

    ex10

---
+++ ex10-udo4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex10_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 100; a++)
+            {
+                if ( a % 3 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+                
+  
