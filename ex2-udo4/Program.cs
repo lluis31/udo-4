a41afdca313ee65ad47ec0bea03a72eac14c2a0f
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 11:32:34 +0100

    ex2

---
+++ ex2-udo4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex2_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe una ciudad");
+            String ciudad = Console.ReadLine();
+
+            Console.WriteLine ("Hola" + nombre + "bienvenido a" + ciudad);
+
+            Console.ReadLine ();
+        }
+    }
+}
