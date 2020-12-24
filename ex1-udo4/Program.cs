e4f829ab6e3147868cecef80e885f540c45db153
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 11:14:51 +0100

    ex1

---
+++ ex1-udo4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex1_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 10;
+            int C = 15;
+
+            int suma = A + B + C;
+
+            Console.WriteLine("Variables A={0}, B={1}, C={2}, La suma de las tres variables es {3}", A, B, C, suma);
+        }
+    }
+}
