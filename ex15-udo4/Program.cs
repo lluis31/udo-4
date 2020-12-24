0e24f5c65ab2463ac497552bb8c9cdd0b1aeab68
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 16:29:30 +0100

    ex15

---
+++ ex15-udo4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex15_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int esDivisible(int numero, int divisible)
+                {
+                    if (numero / divisible == 0)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+        
+
