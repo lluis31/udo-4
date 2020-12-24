579eac33e85912f6b0a0bafede25f15899a1b338
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-24 12:21:27 +0100

    ex3

---
+++ ex3-udo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex3_udo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Escribe tu nombre");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine ("Escribe tu edad");
+            String edad = Console.ReadLine();
+
+            Console.WriteLine("Te llamas" + nombre + "y tienes" + edad + "años"); 
+
+            Console.ReadLine();
+        }
+    }
+}
