06d33c79bc4aef4dc334cbde559ef377fa495a12
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-28 14:15:52 +0100

    ex20

---
+++ ex20-udo4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex20_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+
+             Console.WriteLine("Numero de ventas?");
+
+            int numeroVentas = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numeroVentas; i++)
+            {
+                Console.WriteLine("El precio de venta es n * {0}", i + 1);
+
+                double precioVenta = Convert.ToDouble(Console.ReadLine());
+
+                total = precioVenta + total;
+            }
+             Console.WriteLine("Numero total {0}", total);
+        }
+    }
+}
