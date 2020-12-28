545e65950879a8b806b3328893bc4b835f2bc3d5
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-28 14:26:40 +0100

    ex22

---
+++ ex22-udo4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex22_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contrasenya = "abrete";
+            bool v = false;
+            do
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("\nIntroduzca la contraseña:");
+                    string inPass = Console.ReadLine();
+
+                    if (inPass != contrasenya)
+                    {
+                        Console.WriteLine("Contraseña incorrecta");
+                    }
+                    else
+                    {
+                        i = 4;  //No acierta, sale del bucle for antes de tiempo
+                        Console.WriteLine("Contraseña correcta, Enhorabuena!");
+                        v = true; //Si acierta, sale del bucle while
+
+                    }
+
+                    v = true;
+                }
+            } while (!v);
+        }
+    }
+}
