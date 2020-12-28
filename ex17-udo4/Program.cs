2dbf47ce4141150b66416394c70c53601c4adba7
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-28 12:55:17 +0100

    ex17

---
+++ ex17-udo4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex17_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+            while (x <= 100)
+            {
+                Console.Write(x);
+                Console.Write(" - ");
+                x = x + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
