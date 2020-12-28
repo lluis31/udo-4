3c41a81a802f359506a5d144a4f10378f131432b
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-28 13:28:00 +0100

    ex19

---
+++ ex19-udo4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex19_udo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            {
+                Console.ReadLine();
+            }
+        }
+            
+    }
+ }
+
